55af4b53042f4e4477d790d6c8857833e72cb705
David Dyer <davidrossdyer@gmail.com>
2021-09-28 12:54:10 -0400

    added all develop docs

---
+++ script.js
@@ -0,0 +1,70 @@
+// WINDY 
+https://assets4.lottiefiles.com/temp/lf20_VAmWRg.json
+https://lottiefiles.com/4806-weather-windy
+<iframe src="https://embed.lottiefiles.com/animation/4806"></iframe>
+
+// THUNDER 
+https://assets4.lottiefiles.com/temp/lf20_Kuot2e.json
+https://lottiefiles.com/4805-weather-thunder
+<iframe src="https://embed.lottiefiles.com/animation/4805"></iframe>
+
+// SUNNY 
+https://assets4.lottiefiles.com/temp/lf20_Stdaec.json
+https://lottiefiles.com/4804-weather-sunny
+<iframe src="https://embed.lottiefiles.com/animation/4804"></iframe>
+
+// STORMY 
+https://assets4.lottiefiles.com/temp/lf20_XkF78Y.json
+https://lottiefiles.com/4803-weather-storm
+<iframe src="https://embed.lottiefiles.com/animation/4803"></iframe>
+
+// SUNNY SNOW  
+https://assets4.lottiefiles.com/temp/lf20_BSVgyt.json
+https://lottiefiles.com/4802-weather-snow-sunny
+<iframe src="https://embed.lottiefiles.com/animation/4802"></iframe>
+
+// PARTLY RAINY 
+https://assets4.lottiefiles.com/temp/lf20_rpC1Rd.json
+https://lottiefiles.com/4801-weather-partly-shower
+<iframe src="https://embed.lottiefiles.com/animation/4801"></iframe>
+
+// PARTLY CLOUDY 
+https://assets4.lottiefiles.com/temp/lf20_dgjK9i.json
+https://lottiefiles.com/4800-weather-partly-cloudy
+<iframe src="https://embed.lottiefiles.com/animation/4800"></iframe>
+
+// CLEAR NIGHT 
+https://assets4.lottiefiles.com/temp/lf20_y6mY2A.json
+https://lottiefiles.com/4799-weather-night
+<iframe src="https://embed.lottiefiles.com/animation/4799"></iframe>
+
+// SNOWY NIGHT 
+https://assets4.lottiefiles.com/temp/lf20_RHbbn6.json
+https://lottiefiles.com/4798-weather-snownight
+<iframe src="https://embed.lottiefiles.com/animation/4798"></iframe>
+
+// RAINY NIGHT
+https://assets4.lottiefiles.com/temp/lf20_I5XMi9.json
+https://lottiefiles.com/4797-weather-rainynight
+<iframe src="https://embed.lottiefiles.com/animation/4797"></iframe>
+
+// CLOUDY NIGHT 
+https://assets4.lottiefiles.com/temp/lf20_Jj2Qzq.json
+https://lottiefiles.com/4796-weather-cloudynight
+<iframe src="https://embed.lottiefiles.com/animation/4796"></iframe>
+
+// MISTY OR FOGGY
+https://assets4.lottiefiles.com/temp/lf20_kOfPKE.json
+https://lottiefiles.com/4795-weather-mist
+<iframe src="https://embed.lottiefiles.com/animation/4795"></iframe>
+
+// SNOWY
+https://assets4.lottiefiles.com/temp/lf20_WtPCZs.json
+https://lottiefiles.com/4793-weather-snow
+<iframe src="https://embed.lottiefiles.com/animation/4793"></iframe>
+
+// DAY STORM SHOWERS
+https://assets4.lottiefiles.com/temp/lf20_JA7Fsb.json
+https://lottiefiles.com/4792-weather-stormshowersday
+<iframe src="https://embed.lottiefiles.com/animation/4792"></iframe>
+
