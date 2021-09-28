b830783460a2e1df7f48ef895dcca12c2fff19e4
David Dyer <davidrossdyer@gmail.com>
2021-09-28 18:04:32 -0400

    created nearly all html, secured API connection

--- script.js
+++ script.js
@@ -1,70 +1,19 @@
-// WINDY 
-https://assets4.lottiefiles.com/temp/lf20_VAmWRg.json
-https://lottiefiles.com/4806-weather-windy
-<iframe src="https://embed.lottiefiles.com/animation/4806"></iframe>
+// location: Toccoa GA
+var requestWeather =
+  "https://api.openweathermap.org/data/2.5/onecall?lat=34.5773&lon=-83.3324&exclude=alerts&appid=45ca50dc39eb346ed62587b3b0c97470";
 
-// THUNDER 
-https://assets4.lottiefiles.com/temp/lf20_Kuot2e.json
-https://lottiefiles.com/4805-weather-thunder
-<iframe src="https://embed.lottiefiles.com/animation/4805"></iframe>
+function getApi(requestWeather) {
+  fetch(requestWeather)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+  return data;
+}
 
-// SUNNY 
-https://assets4.lottiefiles.com/temp/lf20_Stdaec.json
-https://lottiefiles.com/4804-weather-sunny
-<iframe src="https://embed.lottiefiles.com/animation/4804"></iframe>
+var responseDetail = getApi(requestWeather);
 
-// STORMY 
-https://assets4.lottiefiles.com/temp/lf20_XkF78Y.json
-https://lottiefiles.com/4803-weather-storm
-<iframe src="https://embed.lottiefiles.com/animation/4803"></iframe>
-
-// SUNNY SNOW  
-https://assets4.lottiefiles.com/temp/lf20_BSVgyt.json
-https://lottiefiles.com/4802-weather-snow-sunny
-<iframe src="https://embed.lottiefiles.com/animation/4802"></iframe>
-
-// PARTLY RAINY 
-https://assets4.lottiefiles.com/temp/lf20_rpC1Rd.json
-https://lottiefiles.com/4801-weather-partly-shower
-<iframe src="https://embed.lottiefiles.com/animation/4801"></iframe>
-
-// PARTLY CLOUDY 
-https://assets4.lottiefiles.com/temp/lf20_dgjK9i.json
-https://lottiefiles.com/4800-weather-partly-cloudy
-<iframe src="https://embed.lottiefiles.com/animation/4800"></iframe>
-
-// CLEAR NIGHT 
-https://assets4.lottiefiles.com/temp/lf20_y6mY2A.json
-https://lottiefiles.com/4799-weather-night
-<iframe src="https://embed.lottiefiles.com/animation/4799"></iframe>
-
-// SNOWY NIGHT 
-https://assets4.lottiefiles.com/temp/lf20_RHbbn6.json
-https://lottiefiles.com/4798-weather-snownight
-<iframe src="https://embed.lottiefiles.com/animation/4798"></iframe>
-
-// RAINY NIGHT
-https://assets4.lottiefiles.com/temp/lf20_I5XMi9.json
-https://lottiefiles.com/4797-weather-rainynight
-<iframe src="https://embed.lottiefiles.com/animation/4797"></iframe>
-
-// CLOUDY NIGHT 
-https://assets4.lottiefiles.com/temp/lf20_Jj2Qzq.json
-https://lottiefiles.com/4796-weather-cloudynight
-<iframe src="https://embed.lottiefiles.com/animation/4796"></iframe>
-
-// MISTY OR FOGGY
-https://assets4.lottiefiles.com/temp/lf20_kOfPKE.json
-https://lottiefiles.com/4795-weather-mist
-<iframe src="https://embed.lottiefiles.com/animation/4795"></iframe>
-
-// SNOWY
-https://assets4.lottiefiles.com/temp/lf20_WtPCZs.json
-https://lottiefiles.com/4793-weather-snow
-<iframe src="https://embed.lottiefiles.com/animation/4793"></iframe>
-
-// DAY STORM SHOWERS
-https://assets4.lottiefiles.com/temp/lf20_JA7Fsb.json
-https://lottiefiles.com/4792-weather-stormshowersday
-<iframe src="https://embed.lottiefiles.com/animation/4792"></iframe>
-
+// var date = responseDetail.current.dt;
+console.log(responseDetail);
