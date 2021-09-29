7a318320e8903b99a377bd62e4f7a7162b38001b
David Dyer <davidrossdyer@gmail.com>
2021-09-28 21:38:02 -0400

    api issue

--- script.js
+++ script.js
@@ -17,3 +17,17 @@ var responseDetail = getApi(requestWeather);
 
 // var date = responseDetail.current.dt;
 console.log(responseDetail);
+
+// var x = document.getElementById("demo");
+//       function getLocation() {
+//         if (navigator.geolocation) {
+//           navigator.geolocation.getCurrentPosition(showPosition);
+//         } else {
+//           x.innerHTML = "Geolocation is not supported by this browser.";
+//         }
+//       }
+
+//       function showPosition(position) {
+//         x.innerHTML = "Latitude: " + position.coords.latitude +
+//         "<br>Longitude: " + position.coords.longitude;
+//       }
