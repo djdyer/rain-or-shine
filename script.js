94867bfc16b48699aff17e4d2aa0d9d25b3b3ffc
David Dyer <davidrossdyer@gmail.com>
2021-09-30 18:59:19 -0400

    fixed all data printing to screen, uvi index sorted

--- script.js
+++ script.js
@@ -75,6 +75,39 @@ function getUVindex(lat, lon) {
     });
 }
 
+// Prints all values to main card
+function printMainData(name, temp, humidity, speed) {
+  $("#city").text(name);
+  $("section").attr("style", "display:inline");
+  var tempF = Math.round((temp - 273.15) * 1.8 + 32);
+  $("#temp").text(tempF + "°");
+  var wind = Math.round(speed);
+  $("#wind").text(wind + "mph");
+  $("#humid").text(humidity + "%");
+}
+
+function printUVData(uvi) {
+  $("#uvi").text(uvi);
+  color = "";
+  var uviRound = Math.round(uvi);
+  switch (true) {
+    case uviRound <= 2:
+      color = "green";
+      break;
+    case uviRound === 3 || uviRound === 4:
+      color = "yellow";
+      break;
+    case uviRound === 5 || uviRound === 6:
+      color = "orange";
+      break;
+    default:
+      color = "red";
+  }
+
+  // Set uvi color based on switch result above
+  $("#uvi").attr("style", "background-color:" + color);
+}
+
 // Gets the 5 day forecast
 function getForecast(city) {
   var requestUrl =
@@ -88,66 +121,73 @@ function getForecast(city) {
     .then(function (data) {
       for (var i = 0; i < data.list.length; i++) {
         if (data.list[i].dt_txt.indexOf("12:00:00") > 0) {
-          console.log(data.list[i]); // weather at noon for next 5 days
+          console.log(data.list[i]);
+          printDay2(
+            data.list[0].main.temp,
+            data.list[0].main.humidity,
+            data.list[0].wind.speed
+          );
+          printDay3(
+            data.list[1].main.temp,
+            data.list[1].main.humidity,
+            data.list[1].wind.speed
+          );
+          printDay4(
+            data.list[2].main.temp,
+            data.list[2].main.humidity,
+            data.list[2].wind.speed
+          );
+          printDay5(
+            data.list[3].main.temp,
+            data.list[3].main.humidity,
+            data.list[3].wind.speed
+          );
+          printDay6(
+            data.list[4].main.temp,
+            data.list[4].main.humidity,
+            data.list[4].wind.speed
+          );
         }
       }
     });
 }
 
-function printMainData(name, temp, humidity, speed) {
-  console.log(name);
-  var nameEl = $("#city").text(name);
-  var populate = $("section").attr("style", "display:inline");
+function printDay2(temp, humidity, speed) {
   var tempF = Math.round((temp - 273.15) * 1.8 + 32);
-  var tempEl = $("#temp").text(tempF + " °");
-  var windEl = $("#wind").text(speed + " MPH");
-  var humidityEl = $("#humidity").text(humidity);
+  $("#temp2").text(tempF + "°");
+  var wind = Math.round(speed);
+  $("#wind2").text(wind + "mph");
+  $("#humid2").text(humidity + "%");
 }
 
-function printUVData(uvi) {
-  var uviEl = $("#uvi").text(uvi);
+function printDay3(temp, humidity, speed) {
+  var tempF = Math.round((temp - 273.15) * 1.8 + 32);
+  $("#temp3").text(tempF + "°");
+  var wind = Math.round(speed);
+  $("#wind3").text(wind + "mph");
+  $("#humid3").text(humidity + "%");
 }
 
-var result1 = $("#result1");
-
-// Day 1
-var cityName = $("#city");
-var day1icon = $("#day1icon");
-var day1temp = $("#temp");
-var day1wind = $("#wind");
-var day1humid = $("#humid");
-var day1uvi = $("#uvi");
-
-function applyDay1() {
-  console.log();
+function printDay4(temp, humidity, speed) {
+  var tempF = Math.round((temp - 273.15) * 1.8 + 32);
+  $("#temp4").text(tempF + "°");
+  var wind = Math.round(speed);
+  $("#wind4").text(wind + "mph");
+  $("#humid4").text(humidity + "%");
 }
 
-// Day 2
-var day2icon = $("#day1icon");
-var day2temp = $("#temp");
-var day2wind = $("#wind");
-var day2humid = $("#humid");
+function printDay5(temp, humidity, speed) {
+  var tempF = Math.round((temp - 273.15) * 1.8 + 32);
+  $("#temp5").text(tempF + "°");
+  var wind = Math.round(speed);
+  $("#wind5").text(wind + "mph");
+  $("#humid5").text(humidity + "%");
+}
 
-// Day 3
-var day3icon = $("#day1icon");
-var day3temp = $("#temp");
-var day3wind = $("#wind");
-var day3humid = $("#humid");
-
-// Day 4
-var day4icon = $("#day1icon");
-var day4temp = $("#temp");
-var day4wind = $("#wind");
-var day4humid = $("#humid");
-
-// Day 5
-var day5icon = $("#day1icon");
-var day5temp = $("#temp");
-var day5wind = $("#wind");
-var day5humid = $("#humid");
-
-// Day 6
-var day6icon = $("#day1icon");
-var day6temp = $("#temp");
-var day6wind = $("#wind");
-var day6humid = $("#humid");
+function printDay6(temp, humidity, speed) {
+  var tempF = Math.round((temp - 273.15) * 1.8 + 32);
+  $("#temp6").text(tempF + "°");
+  var wind = Math.round(speed);
+  $("#wind6").text(wind + "mph");
+  $("#humid6").text(humidity + "%");
+}
