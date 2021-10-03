b3c7080bd89cfb16bb2e2c0c6bda391aaf2e45a2
David Dyer <davidrossdyer@gmail.com>
2021-10-03 15:51:34 -0400

    working out darkmode, and gif index

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+$("body").attr("style", "background-color:rgb(21, 21, 21)");
+$("header").attr("style", "background-image: url(./assets/night-sky.png)");
+$("h1").attr("style", "color:rgb(201, 201, 201)");
+$("h2").attr("style", "color:rgb(201, 201, 201)");
+$("h3").attr("style", "color:rgb(201, 201, 201)");
+$("h4").attr("style", "color:rgb(201, 201, 201)");
+$("h5").attr("style", "color:rgb(201, 201, 201)");
+$("h6").attr("style", "color:rgb(201, 201, 201)");
+$("#input").attr(
+  "style",
+  "color:white; border: 1px solid #333; background-color:#222"
+);
+$("#search").attr("style", "background-color:black");
+$(".results").attr("style", "border-top: 1px solid #333");
+$("#result1").attr("style", "color:white; background-color:#222");
+$("#card").attr(
+  "style",
+  "background-color:#222; box-shadow: 0px 8px 30px rgb(0, 0, 0)"
+);
+$("#subhead").attr("style", "border-top: 1px solid #333; color:white");
+$(".small-card").attr(
+  "style",
+  "background-color:#222; box-shadow:0px 8px 30px rgb(0, 0, 0)"
+);
+$("footer").attr("style", "background-color:#111");
+$("#closeBtn").attr(
+  "style",
+  "background-color:black; border:1px solid transparent; color:white"
+);
+$("a").attr("style", "color: rgb(63, 63, 63)");
+
 // Sets all dates
 var day1 = moment();
 $("#date").text(day1.format("dddd, MMM Do YYYY"));
@@ -12,6 +43,7 @@ $("#day5").text(day5.format("ddd, MMM Do"));
 var day6 = moment().add(5, "d");
 $("#day6").text(day6.format("ddd, MMM Do"));
 
+var timeOfDay = moment().format("A");
 var input = $("#input");
 
 // Search button click or enter key fires newSearch function with city iput
@@ -54,6 +86,9 @@ function getCurrentWeather(city) {
         data.main.humidity,
         data.wind.speed
       );
+      saveSearch(data.name);
+      // printGifs(data.weather[0].id);
+      // console.log(data.weather[0].id);
     });
 }
 
@@ -76,7 +111,7 @@ function getUVindex(lat, lon) {
 }
 
 // Prints all values to main card
-function printMainData(name, temp, humidity, speed) {
+function printMainData(name, temp, humidity, speed, id) {
   $("#city").text(name);
   $("section").attr("style", "display:inline");
   var tempF = Math.round((temp - 273.15) * 1.8 + 32);
@@ -103,7 +138,6 @@ function printUVData(uvi) {
     default:
       color = "red";
   }
-
   // Set uvi color based on switch result above
   $("#uvi").attr("style", "background-color:" + color);
 }
@@ -147,6 +181,13 @@ function getForecast(city) {
             data.list[4].main.humidity,
             data.list[4].wind.speed
           );
+          // printGifs(
+          //   data.list[0].weather.id,
+          //   data.list[1].weather.id,
+          //   data.list[2].weather.id,
+          //   data.list[3].weather.id,
+          //   data.list[4].weather.id
+          // );
         }
       }
     });
@@ -191,3 +232,94 @@ function printDay6(temp, humidity, speed) {
   $("#wind6").text(wind + "mph");
   $("#humid6").text(humidity + "%");
 }
+
+// Saving the search to local storage, displaying history under search
+function saveSearch(name) {
+  localStorage.setItem("Search1", name);
+  $("#results-header").attr("style", "display:flex");
+  $("#result1").attr("style", "display:inline");
+  $("#result1").text(name);
+}
+
+// 200 thunderstorms
+var thunderDay = "./assets/day-gifs/weather-thunder.gif";
+var thunderNight = "./assets/night-gifs/thunder-night.gif";
+
+// 201 thunderstorms rain
+// 202 thunderstorms heavy rain
+// 230 thunderstorms light rain
+// 231 thunderstorms drizzle
+// 232 thunderstorms heavy drizzle
+// 233 thunderstorms with hail
+var stormDay = "./assets/day-gifs/weather-storm.gif";
+var stormNight = "./assets/night-gifs/storm-night.gif";
+
+// 302 heavy drizzle
+// 500 light rain
+// 502 heavy rain
+// 511 freezing rain
+// 521 shower rain
+// 522 heavy shower rain
+var heavyRainDay = "./assets/day-gifs/storm-showers-day.gif";
+var heavyRainNight = "./assets/night-gifs/partly-shower-night.gif";
+
+// 501 moderate rain
+// 520 light shower rain
+// 300 light drizzle
+// 301 drizzle
+var lightRainDay = "./assets/day-gifs/weather-partly-shower.gif";
+var lightRainNight = "./assets/night-gifs/partly-shower-night.gif";
+
+// 601 snow
+// 602 heavy snow
+// 610 mix snow/rain
+// 611 sleet
+// 612 heavy sleet
+// 622 heavy snow shower
+var snowDay = "./assets/day-gifs/weather-snow-sunny.gif";
+var snowNight = "./assets/night-gifs/partly-snow-night.gif";
+
+// 623 flurries
+// 600 light snow
+// 621 snow shower
+var partlySnowDay = "./assets/day-gifs/weather-snow.gif";
+var partlySnowNight = "./assets/night-gifs/snow-night.gif";
+
+// 700 mist
+// 711 smoke
+// 721 haze
+// 731 sand/dust
+// 741 fog
+// 751 freezing fog
+// 900 unknown percipitation
+var mistDay = "./assets/day-gifs/weather-mist.gif";
+var mistNight = "./assets/night-gifs/mist-night.gif";
+
+// 800 clear sky
+var clearDay = "./assets/day-gifs/weather-sunny.gif";
+var clearNight = "./assets/night-gifs/clear-night.gif";
+
+// 801 few clouds
+// 802 scattered clouds
+// 803 broken clouds
+var partlyCloudyDay = "./assets/day-gifs/weather-partly-cloudy.gif";
+var partlyCloudyNight = "./assets/night-gifs/partly-cloudy-night.gif";
+
+// 804 overcast clouds
+var windyDay = "./assets/day-gifs/weather-windy.gif";
+var windyNight = "./assets/night-gifs/windy-night.gif";
+
+// function printGifs(id) {
+//   switch (true) {
+//     case id === 800:
+//       $(`<img src="$("./assets/night-gifs/clear-night.gif")">`).appendTo(
+//         "#day1icon"
+//       );
+//       break;
+//     case id === 201 || 202 || 230 || 231 || 232 || 233:
+//       $(`<img src="$("./assets/night-gifs/storm-night.gif")">`).appendTo(
+//         "#day1icon"
+//       );
+//       break;
+//   }
+// }
